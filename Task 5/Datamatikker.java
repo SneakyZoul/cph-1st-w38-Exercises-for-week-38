138cc2a521f7e90b87db34340cc0d1c92e19e966
SneakyZoul <78617698+SneakyZoul@users.noreply.github.com>
2021-02-25 20:27:00 +0100

    opg

---
+++ Task 5/Datamatikker.java
@@ -0,0 +1,29 @@
+class Datamatikker 
+{
+   
+
+
+    public static void main(String [] args)
+    {
+        allStudents();
+    }
+
+
+    public static void allStudents(string[] students)
+    {
+        int [] students = new students[10];
+        students [0] = new Student("olle", 22, false, "spring");
+        students [1] = new Student("Nicolaj", 20, false, "spring";
+        students [2] = new Student("Mie", 21, true, "spring");
+        students [3] = new Student("jesper", 35, false, "spring");
+        students [4] = new Student("Mikkel", 22, false, "spring");
+        students [5] = new Student("Asger", 23, false, "Spring");
+        students [6] = new Student("alexander", 26, false, "spring");
+        students [7] = new Student("tess", 38, true, "spring");
+        students [8] = new Student("kim", 37, false, "spring");
+        students [9] = new Student("jantie", 22, true, "spring"); 
+    }
+    
+        
+
+}
---
+++ Task 5/Student.java
@@ -0,0 +1,19 @@
+class Student 
+{
+    int [] students = new students[10];
+    public String name; 
+    public int age;
+    public boolean isFemale;
+    public String datamatikerTeam; 
+
+
+public Student(String Name,int Age, boolean IsFemale, String DatamatikerTeam)
+{
+    this.name = Name; 
+    this.age = Age; 
+    this.isFemale = IsFemale; 
+    this.datamatikerTeam = DatamatikerTeam; 
+
+}
+    
+}
